2af2c7d3296bc6c9851ab0bc351a6d42811622f6
NoraTiku <jtiku@madisoncollege.edu>
2022-03-07 21:37:41 -0600

    Got the bitcoin app working...

--- src/App.js
+++ src/App.js
@@ -7,32 +7,60 @@ import { API } from 'aws-amplify'
 import './App.css';
 
 const App = () => {
-  // Create coins variable and set to empty array
-  const [coins, updateCoins] = useState([]);
 
-  // Define function to all API
-  const fetchCoins = async () => {
-    const data = await API.get('cryptoapi22s', '/coins');
-    updateCoins(data.coins);
-  };
 
-  // Call fetchCoins function when component loads
-  useEffect(() => {
-    fetchCoins();
-  }, []);
+    // Create additional state to hold user input for limit and start properties
+    const [input, updateInput] = useState({ limit: 5, start: 0 });
 
-  return (
-    <div className="App">
-      {
-        coins.map((coin) => (
-          <div key={coin.name}>
-            <h2>{coin.name} - {coin.symbol}</h2>
-            <h5>${coin.price_usd}</h5>
-          </div>
-        ))
-      }
-    </div>
-  );
+    // Create a new function to allow users to update the input values
+    const  updateInputValues = (type, value) => {
+    updateInput({ ...input, [type]: value });
+    };
+
+    // Create coins variable and set to empty array
+    const [coins, updateCoins] = useState([]);
+
+    // Define function to all API
+    const fetchCoins = async () => {
+      const { limit, start } = input;
+      const data = await API.get('cryptoapi22s', `/coins?limit=${limit}&start=${start}`);
+      updateCoins(data.coins);
+    }
+
+
+    // Call fetchCoins function when component loads
+    useEffect(() => {
+      fetchCoins();
+    }, []);
+
+
+    return (
+      <div className="App">
+          <input
+              onChange={e => updateInputValues('limit', e.target.value)}
+              placeholder="Enter a limit"
+          />
+        <input
+              placeholder="Enter a starting value"
+              onChange={e => updateInputValues('start', e.target.value)}
+        />
+
+              
+              <button onClick={fetchCoins}>Fetch Coins</button>
+              
+
+
+
+        {
+          coins.map((coin) => (
+            <div key={coin.name}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        }
+      </div>
+    );
 }
 
 export default App
